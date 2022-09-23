b6f5c7edc0b1fb296ba2c6eeaaddd91e4039ccde
EGaroufalias <eliasgaroufalias@gmail.com>
2022-09-23 17:36:18 +0000

    changed route from users/sign_in to /login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
   devise_for :users
     root to: 'pages#index'
-  end
-  
+    
+end
