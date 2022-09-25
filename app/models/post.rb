3ec465b420b41eedb6306c8ccc8cb20ba87d358e
EGaroufalias <eliasgaroufalias@gmail.com>
2022-09-25 11:32:27 +0000

    Create Private::Conversation and Private::Message models - Define associations between User, Private::Conversation   and Private::Message models - Define private_conversations and private_messages tables

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,5 +17,5 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { minimum: 5, maximum: 255 }
     validates :content, presence: true, length: { minimum: 20, maximum: 1000 }
     validates :category_id, presence: true
-    
-  end
+
+end
